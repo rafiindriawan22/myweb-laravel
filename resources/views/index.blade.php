42540c67269026ed6d7cb3f0ab390f443f6eb683
rafiindriawan22 <indriawan.rafimuhammad@gmail.com>
2021-12-10 00:03:22 +0700

    push all file to git

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Setup Laravel Bootstrap Successfully <i class="fas fa-ad"></i></h1>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html> --}}
+
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('pages.header')
+            @include('pages.project')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
+</html>
